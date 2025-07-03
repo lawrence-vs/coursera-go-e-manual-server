69428c8ac23388dc343a7db702c27807d168969b
LJ van Staden <lawrence@luno.com>
2025-07-03 22:36:32 +0200

    finished graded assignment

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -21,8 +22,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, pages)
 }
 
+func newsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("recieved: " + r.URL.Path)
+	t, err := template.ParseFiles("templates/news.html")
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%v Server error\n", http.StatusNotFound)
+		fmt.Fprintf(w, "Description: %s\n", err)
+		return
+	}
+	// get today's 2025-07-03
+	date := time.Now().String()
+	t.Execute(w, date)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/news", newsHandler)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/manuals/", http.StripPrefix("/manuals/", http.FileServer(http.Dir("manuals"))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 	fmt.Println("Listening on port 30000...")
 	http.ListenAndServe(":3000", nil)
 }
